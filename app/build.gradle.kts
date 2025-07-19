d912d74c5b18eaa066d6cd95e4a8880d0e06d2ec
7ree <350913+7ree@user.noreply.gitee.com>
2025-07-19 17:05:26 +0800

    build:更新说明文件中版本要点，更新app版本号至3.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.x7ree.wordcard"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "3.7"
+        versionCode = 11
+        versionName = "3.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
