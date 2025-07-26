95f91330decae91a65362da04efd0167b8da1784
7ree <350913+7ree@user.noreply.gitee.com>
2025-07-27 00:22:59 +0800

    feat(changelog): 更新 CHANGELOG.md 以反映 v5.3.0 版本的改进
    - 添加了 v5.3.0 版本的功能改进、界面优化和技术改进
    - 更新了 README.md 中的版本号徽章
    - 在 build.gradle.kts 中更新了版本代码和版本名称

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.x7ree.wordcard"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20
-        versionName = "5.2"
+        versionCode = 21
+        versionName = "5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
